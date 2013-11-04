2277e1663a0c2976a56506a0b5021fd3a45e51d2
Prateek Malhotra <someone1@gmail.com>
2013-11-03 23:24:39 -0500

    Forgot file from previous commit

--- app.go
+++ app.go
@@ -1,12 +1,7 @@
 package beego
 
 import (
-	"fmt"
 	"github.com/astaxie/beego/context"
-	"net"
-	"net/http"
-	"net/http/fcgi"
-	"time"
 )
 
 type FilterFunc func(*context.Context)
@@ -20,61 +15,6 @@ func NewApp() *App {
 	cr := NewControllerRegistor()
 	app := &App{Handlers: cr}
 	return app
-}
-
-func (app *App) Run() {
-	addr := HttpAddr
-
-	if HttpPort != 0 {
-		addr = fmt.Sprintf("%s:%d", HttpAddr, HttpPort)
-	}
-	var (
-		err error
-		l   net.Listener
-	)
-	if UseFcgi {
-		if HttpPort == 0 {
-			l, err = net.Listen("unix", addr)
-		} else {
-			l, err = net.Listen("tcp", addr)
-		}
-		if err != nil {
-			BeeLogger.Critical("Listen: ", err)
-		}
-		err = fcgi.Serve(l, app.Handlers)
-	} else {
-		if EnableHotUpdate {
-			server := &http.Server{
-				Handler:      app.Handlers,
-				ReadTimeout:  time.Duration(HttpServerTimeOut) * time.Second,
-				WriteTimeout: time.Duration(HttpServerTimeOut) * time.Second,
-			}
-			laddr, err := net.ResolveTCPAddr("tcp", addr)
-			if nil != err {
-				BeeLogger.Critical("ResolveTCPAddr:", err)
-			}
-			l, err = GetInitListner(laddr)
-			theStoppable = newStoppable(l)
-			err = server.Serve(theStoppable)
-			theStoppable.wg.Wait()
-			CloseSelf()
-		} else {
-			s := &http.Server{
-				Addr:         addr,
-				Handler:      app.Handlers,
-				ReadTimeout:  time.Duration(HttpServerTimeOut) * time.Second,
-				WriteTimeout: time.Duration(HttpServerTimeOut) * time.Second,
-			}
-			if HttpTLS {
-				err = s.ListenAndServeTLS(HttpCertFile, HttpKeyFile)
-			} else {
-				err = s.ListenAndServe()
-			}
-		}
-	}
-	if err != nil {
-		BeeLogger.Critical("ListenAndServe: ", err)
-	}
 }
 
 func (app *App) Router(path string, c ControllerInterface, mappingMethods ...string) *App {
