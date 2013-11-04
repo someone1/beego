0b99f32423f27e2c1f24d1383f815d85b1635c6d
Prateek Malhotra <someone1@gmail.com>
2013-11-03 23:59:21 -0500

    Fixes for appengine on configuration section

--- config.go
+++ config.go
@@ -4,9 +4,6 @@ import (
 	"github.com/astaxie/beego/config"
 	"github.com/astaxie/beego/session"
 	"html/template"
-	"os"
-	"path"
-	"runtime"
 	"strconv"
 	"strings"
 )
@@ -54,44 +51,6 @@ var (
 	TemplateRight         string
 	BeegoServerName       string
 )
-
-func init() {
-	os.Chdir(path.Dir(os.Args[0]))
-	BeeApp = NewApp()
-	AppPath = path.Dir(os.Args[0])
-	StaticDir = make(map[string]string)
-	TemplateCache = make(map[string]*template.Template)
-	HttpAddr = ""
-	HttpPort = 8080
-	AppName = "beego"
-	RunMode = "dev" //default runmod
-	AutoRender = true
-	RecoverPanic = true
-	PprofOn = false
-	ViewsPath = "views"
-	SessionOn = false
-	SessionProvider = "memory"
-	SessionName = "beegosessionID"
-	SessionGCMaxLifetime = 3600
-	SessionSavePath = ""
-	SessionHashFunc = "sha1"
-	SessionHashKey = "beegoserversessionkey"
-	SessionCookieLifeTime = 3600
-	UseFcgi = false
-	MaxMemory = 1 << 26 //64MB
-	EnableGzip = false
-	StaticDir["/static"] = "static"
-	AppConfigPath = path.Join(AppPath, "conf", "app.conf")
-	HttpServerTimeOut = 0
-	ErrorsShow = true
-	XSRFKEY = "beegoxsrf"
-	XSRFExpire = 0
-	TemplateLeft = "{{"
-	TemplateRight = "}}"
-	BeegoServerName = "beegoServer"
-	ParseConfig()
-	runtime.GOMAXPROCS(runtime.NumCPU())
-}
 
 func ParseConfig() (err error) {
 	AppConfig, err = config.NewConfig("ini", AppConfigPath)
---
+++ configinit_appengine.go
@@ -0,0 +1,44 @@
+// +build appengine
+
+package beego
+
+import (
+	"html/template"
+	"path"
+)
+
+func init() {
+	BeeApp = NewApp()
+	AppPath = ""
+	StaticDir = make(map[string]string)
+	TemplateCache = make(map[string]*template.Template)
+	HttpAddr = ""
+	HttpPort = 8080
+	AppName = "beego"
+	RunMode = "dev" //default runmod
+	AutoRender = true
+	RecoverPanic = true
+	PprofOn = false
+	ViewsPath = path.Join(AppPath, "views")
+	SessionOn = false
+	SessionProvider = "memory"
+	SessionName = "beegosessionID"
+	SessionGCMaxLifetime = 3600
+	SessionSavePath = ""
+	SessionHashFunc = "sha1"
+	SessionHashKey = "beegoserversessionkey"
+	SessionCookieLifeTime = 3600
+	UseFcgi = false
+	MaxMemory = 1 << 26 //64MB
+	EnableGzip = false
+	StaticDir["/static"] = "static"
+	AppConfigPath = path.Join(AppPath, "conf", "app.conf")
+	HttpServerTimeOut = 0
+	ErrorsShow = true
+	XSRFKEY = "beegoxsrf"
+	XSRFExpire = 0
+	TemplateLeft = "{{"
+	TemplateRight = "}}"
+	BeegoServerName = "beegoServer"
+	ParseConfig()
+}
